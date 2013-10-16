add222e0f477697055c7e6cdf97d0ff9e5257cb1
Mitchell Allison <mitchellallison@me.com>
2013-10-16 17:09:23 +0100

    Setting up web.py framework and celery workers.

---
+++ code.py
@@ -0,0 +1,15 @@
+import web
+
+render = web.template.render('templates/')
+
+urls = (
+  '/', 'index'
+)
+
+class index:
+  def GET(self):
+    return render.index()
+
+if __name__ == "__main__":
+  app = web.application(urls, globals())
+  app.run()
