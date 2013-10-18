6b02e010e24418dd5b921e94963cd39b9c219b95
Mitchell Allison <mitchellallison@me.com>
2013-10-18 18:45:54 +0100

    The user interface can now send a string to the server asynchronously and then receive the same string back. Close 1 Close 2

--- code.py
+++ code.py
@@ -6,9 +6,22 @@ urls = (
   '/', 'index'
 )
 
+logic_form = web.form.Form(
+    web.form.Textarea('logic', class_="box_sizing")
+)
+
 class index:
   def GET(self):
-    return render.index('Sam')
+    form = logic_form()
+    return render.index(form);
+
+  def POST(self):
+    form = logic_form()
+    # Validates the Form
+    form.validates()
+    # TODO: We have the logic as a string, we need to process it
+    logic_to_translate = form.logic.get_value()
+    return logic_to_translate;
 
 if __name__ == "__main__":
   app = web.application(urls, globals())
--- static/main.js
+++ static/main.js
@@ -1,1 +1,17 @@
-// Some JS here
+/* When 'Convert to SQL' button is clicked fire off an AJAX request */
+$(document).ready(function() {
+  $('#convert_button').click(function () {
+    var input_string = $("Textarea#logic").val();
+
+    $.ajax({
+      type: "POST",
+      data: {
+        'logic' : input_string
+      }
+    }).done(function(result) {
+      /* Handle the result of the translation */
+      $('#result').text(result);
+    });
+    return false;
+  });
+});
