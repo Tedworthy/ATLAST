8fa3f80da520afaa7ad53c515dd2a3461b29921d
Max Tottenham <mtottenh@gmail.com>
2013-10-21 14:13:49 +0100

    Added ply to be tracked by git. Started work on parser for FOL

---
+++ parser.py
@@ -0,0 +1,37 @@
+import lex
+
+
+tokens = (
+  "FORALL",
+  "THEREEXISTS,
+  "TRUE",
+  "FALSE
+  "VARIABLE",
+  "OR",
+  "AND",
+  "IMPLIES",
+  "IFF",
+  "GT",
+  "LT",
+  "GTE",
+  "LTE",
+  "EQ",
+  "CONNECTIVE",
+  "FUNCTION",
+  "OBRACKET",
+  "CBRACKET",
+  "COMMA",
+  "NOT"
+
+  
+)
+
+
+# Variables          -> used in queries 
+# Constant Symbols   -> used in declarations
+# Functional Symbols -> think addition multiplication subtraction f(x) g(x)
+# Relational Symbols -> functions with arities e.g. hasFather(x,y) 
+# Logical Constants  -> and|OR| NOT| ForAll| ThereExists| Equal| 
+# 
+  
+
