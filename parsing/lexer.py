db290feb46c0668f8b303f4c03e91da4ba0a8bf8
Sam Esgate <sam.esgate11@imperial.ac.uk>
2013-10-21 18:07:21 +0100

    Added some cheeky regexes, very simple for now I'm afraid.

--- parsing/lexer.py
+++ parsing/lexer.py
@@ -5,7 +5,6 @@ tokens = (
   "THEREEXISTS",
   "TRUE",
   "FALSE",
-  "VARIABLE",
   "OR",
   "AND",
   "IMPLIES",
@@ -15,46 +14,40 @@ tokens = (
   "GTE",
   "LTE",
   "EQ",
-  "CONNECTIVE",
-  "FUNCTION",
   "OBRACKET",
   "CBRACKET",
   "COMMA",
   "NOT"
-
-  
+  "VARIABLE",
+  "CONNECTIVE",
+  "FUNCTION",
 )
 
-
-
-t_FORALL = ""
-t_THEREEXISTS = ""
-t_TRUE = ""
-t_FALSE = ""
-t_VARIABLE = ""
-t_OR = ""
-t_AND = "" 
-t_IMPLIES = "" 
-t_IFF = ""
-t_GT = ""
-
-t_LT = ""
-
-t_GTE = ""
-t_LTE = ""
-t_EQ = ""
+t_FORALL = "∀"
+t_THEREEXISTS = "∃"
+t_TRUE = "True"
+t_FALSE = "False"
+t_OR = "|"
+t_AND = "&"
+t_IMPLIES = "→"
+t_IFF = "↔"
+t_GT = ">"
+t_LT = "<"
+t_GTE = "≥"
+t_LTE = "≤"
+t_EQ = "="
+t_OBRACKET = "("
+t_CBRACKET = ")"
+t_COMMA = ","
+t_NOT = "¬"
+# One or more lowercase characters
+t_VARIABLE = r'[a-z]+'
 t_CONNECTIVE = ""
 t_FUNCTION = ""
-t_OBRACKET = ""
-t_CBRACKET = ""
-t_COMMA = ""
-t_NOT = ""
 
 # Variables          -> used in queries 
 # Constant Symbols   -> used in declarations
 # Functional Symbols -> think addition multiplication subtraction f(x) g(x)
 # Relational Symbols -> functions with arities e.g. hasFather(x,y) 
 # Logical Constants  -> and|OR| NOT| ForAll| ThereExists| Equal| 
-# 
-  
-
+#
