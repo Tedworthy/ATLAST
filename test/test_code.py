5aa390d4bf64e3fa3586ae703518a01779596bf3
Max Tottenham <mtottenh@gmail.com>
2013-10-28 11:22:42 +0000

    edited test

--- test/test_code.py
+++ test/test_code.py
@@ -14,8 +14,7 @@ class TestCode():
   def test_query(self):
     middleware=[]
     testApp = TestApp(app.wsgifunc(*middleware))
-    r = testApp.post('/', params={'logic':'test'})
-    assert_equal(r.status, 200)
-    r = r.follow()
+    r = testApp.get('/')
+    form = res.form['logic']
     r.showbrowser()
-    
+
