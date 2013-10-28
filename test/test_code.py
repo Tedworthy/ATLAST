6f84ff93b63c4f41567bd4e354c0944248abc4de
Max Tottenham <mtottenh@gmail.com>
2013-10-28 11:11:42 +0000

    added testcase. at the moment it checks for the same string being returned from a POST request. next step check for a valid translation

--- test/test_code.py
+++ test/test_code.py
@@ -3,10 +3,18 @@ from nose.tools import *
 from code import app
 
 class TestCode():
-    def test_index(self):
-        middleware=[]
-        testApp = TestApp(app.wsgifunc(*middleware))
-        r = testApp.get('/')
-        assert_equal(r.status, 200)
-        r.mustcontain('Convert to SQL')
+  def test_index(self):
+    middleware=[]
+    testApp = TestApp(app.wsgifunc(*middleware))
+    r = testApp.get('/')
+    assert_equal(r.status, 200)
+    r.mustcontain('Convert to SQL')
 
+  def test_query(self):
+    middleware=[]
+    testApp = TestApp(app.wsgifunc(*middleware))
+    r = testApp.post('/', params={'logic':'test'})
+    assert_equal(r.status, 200)
+    r = r.follow()
+    r.showbrowser()
+    
