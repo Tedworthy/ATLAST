049b76a76492b93bb9f553fdedc3c595d846245a
Mitchell Allison <mitchellallison@me.com>
2013-10-29 12:01:29 +0000

    Adding missing files

---
+++ parsing/ast/binary_equality_node.py
@@ -0,0 +1,12 @@
+'''
+BinaryEquality Node
+This class implements the AST node of a first order logic formula, in the form
+formula = formula.
+'''
+
+from node import Node
+
+class BinaryEqualityNode(Node):
+  def __init__(self, left, right):
+    setLeft(left)
+    setRight(right)
