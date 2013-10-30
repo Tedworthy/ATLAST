cf5f8b88310bcc8bd53e08a3b52ee4c3d773b39c
Ted Sales <ted@macrohard.co.uk>
2013-10-30 01:04:22 +0000

    Added basic capability for the UI to replace certain sequences of characters with their equivalent logic Unicode symbols on-the-fly. Seems to work well with limited testing.

--- static/main.js
+++ static/main.js
@@ -1,20 +1,50 @@
 $(document).ready(function() {
 
+  var keys = {
+    "92": { "char": "\\", "formatters": ["and"] },
+    "62": { "char": ">", "formatters": ["implies"] }
+  }
+
+  var formatters = {
+    "and": { "regex": /\/\\/g, "result": "\u2227" },
+    "implies": { "regex": /->/g, "result": "\u2192" }
+  };
+
   /* When 'Convert to SQL' button is clicked fire off an AJAX request */
-  $('#convert_button').click(function () {
+  $("#convert_button").click(function() {
     var input_string = $("textarea#logic").val();
     $.ajax({
       type: "POST",
       data: {
-        'logic' : input_string
+        "logic" : input_string
       }
     }).done(function(result) {
       /* Handle the result of the translation */
-      response = $.parseJSON(result);
-      $('textarea#sql_result').text(response.sql);
-      $('textarea#query_result').text(response.query);
+      var response = $.parseJSON(result);
+      $("textarea#sql_result").text(response.sql);
+      $("textarea#query_result").text(response.query);
     });
     return false;
+  });
+
+  $("textarea#logic").keypress(function(event) {
+    console.log("Key down:" + event.keyCode);
+    var key = keys[event.keyCode.toString()];
+    if (key !== undefined) {
+      event.preventDefault();
+      var matching_formatters = key.formatters.map(function(name) {
+        return formatters[name];
+      });
+      var logic = $(this).val() + key.char;
+      $.each(matching_formatters, function(index, regex_result) {
+        logic = logic.replace(regex_result.regex, regex_result.result);
+      });
+      $(this).val(logic);
+    }
+    //var logic = $(this).val();
+    //if (logic.slice(-1) == "^") {
+    //  $("textarea#sql_result").text("^ FOUND");
+    //}
   });
 
 });
