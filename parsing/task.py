9ec70f01e6e0d84a6fc61da5650273a3cdd729ac
Max Tottenham <mtottenh@gmail.com>
2013-10-31 12:32:39 +0000

    why you no work

--- parsing/task.py
+++ parsing/task.py
@@ -7,5 +7,5 @@ celery = Celery('task', backend='amqp://guest@localhost:5672', broker='amqp://gu
 @celery.task
 def add_to_parse_q(logic_to_translate):
     
-    return parsing.parser.parse_input(logic_to_translate)
+    return parser.parse_input(logic_to_translate)
 
