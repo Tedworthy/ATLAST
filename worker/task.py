34a4ea1c27273d440d2004d579dd0a52a276d22f
Max Tottenham <mtottenh@gmail.com>
2013-10-31 12:06:18 +0000

    working on getting celery working

---
+++ worker/task.py
@@ -0,0 +1,9 @@
+from celery import Celery
+from parsing import *
+celery = Celery('task', backend='amqp://guest@localhost:5672', broker='amqp://guest@localhost')
+
+@celery.task
+def add_to_parse_q(logic_to_translate):
+    
+    return parsing.dat_parsing(logic_to_translate)
+
