0c6859fcd15f08251c578436e05e4d295ff0ca89
Max Tottenham <mtottenh@gmail.com>
2013-10-31 12:37:09 +0000

    WORK DAMN YOU WORK

--- parsing/task.py
+++ parsing/task.py
@@ -1,11 +1,11 @@
 import sys
 from celery import Celery
 
-from parsing import *
+from parser import parse_input
 celery = Celery('task', backend='amqp://guest@localhost:5672', broker='amqp://guest@localhost')
 
 @celery.task
 def add_to_parse_q(logic_to_translate):
     
-    return parser.parse_input(logic_to_translate)
+    return parse_input(logic_to_translate)
 
