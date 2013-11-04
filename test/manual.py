35317d4e405c1ad711b01dee24d11ed52e301e4f
Ted Sales <ted@macrohard.co.uk>
2013-11-04 22:55:31 +0000

    Bit of test reorganisation. Also fixed symbol table generation test (was failing when code was working) - who wrote 'assert False'?\!

--- test/manual.py
+++ test/manual.py
@@ -1,15 +1,17 @@
 import sys
+import os
 import parsing
 from codegen.symtable import SymTable
 from codegen.generic_logic_ast_visitor import GenericLogicASTVisitor
 
-if len(sys.argv) > 0:
-  input_file = sys.argv[1]
-else:
-  print "Incorrect usage. try python parser.py [file_name]"
+if len(sys.argv) <= 1:
+  print "Usage:\n  python {0} <logic_input_file_name>".format(sys.argv[0])
   sys.exit(1)
+elif not os.path.exists(sys.argv[1]):
+  print "ERROR: Specified file '{0}' does not exist".format(sys.argv[1])
+  sys.exit(2)
 
-f = open(input_file, 'r')
+f = open(sys.argv[1], 'r')
 result = parsing.parse_input(f.read().decode('utf8'))
 symbolTable = SymTable()
 astVisitor = GenericLogicASTVisitor()
--- test/test_parser.py
+++ test/test_parser.py
@@ -6,27 +6,16 @@ from codegen.symtable import SymTable
 from codegen.generic_logic_ast_visitor import GenericLogicASTVisitor
 import sys
 
-
 class TestParser():
   def test_io(self):
-    f = open( 'test/input_file.txt', 'r')
+    f = open('test/input_file.txt', 'r')
     assert_equals(f.read(), '∀x(p(x) ∧ q(x))\n')
 
   def test_symboltable_generation(self):
-    f = open( 'test/input_file.txt', 'r')
-    input = f.read().decode('utf8')
+    f = open('test/input_file.txt', 'r')
+    input = f.read().decode('utf-8')
     table = SymTable()
     syntax_tree = parsing.parse_input(input)
     syntax_tree.generateSymbolTable(table)
-    print table
-    assert False
-    
-    
-     
-
-
-
-
-
-
+    assert_equals(str(table), 'q,x,p')
 
