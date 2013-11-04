289f5d67096d96ab7babf963bf1b4ea40097e4db
Max Tottenham <mtottenh@gmail.com>
2013-11-04 15:12:15 +0000

    debugging symboltable generation

--- ast/n_arity_application_node.py
+++ ast/n_arity_application_node.py
@@ -8,13 +8,13 @@ or predicate).
 from node import Node
 
 class NArityApplicationNode(Node):
-  _identifier = None
 
   def __init__(self, identifier):
     Node.__init__(self)
     self._identifier = identifier
 
   def generateSymbolTable(self, symtable):
+    print "WE GOT CALLED "
     if symtable.lookup(self._identifier) is None:
       symtable.addGlobal(self._identifier, self)
 
