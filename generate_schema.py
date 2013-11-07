907ef85e3ef11d0f7f63e86c998e2c68629431e3
Mitchell Allison <mitchellallison@me.com>
2013-11-07 12:10:20 +0000

    Python script now generates an xml schema to be read into an object when running. This is so that we have a mapping of tables to primary keys

--- generate_schema.py
+++ generate_schema.py
@@ -1,17 +1,24 @@
+import xml.etree.cElementTree as ET
+from lxml import etree
 import psycopg2
+
+con = psycopg2.connect(host='axa-prj-03.doc.ic.ac.uk',
+                       port='55432', dbname='filmdb', 
+                       user='link', password='triforce')
 
 table_query = """ SELECT table_name
                   FROM information_schema.tables
                   WHERE table_schema='public'"""
 
-con = psycopg2.connect(host='axa-prj-03.doc.ic.ac.uk',
-                       port='55432', dbname='filmdb', 
-                       user='link', password='triforce')
 cur = con.cursor()
 cur.execute(table_query)
 tables = cur.fetchall()
 
+root = etree.Element("root")
+
 for table in (table[0] for table in tables):
+  xmltable = etree.SubElement(root, "table")
+  xmltable.set("name", table)
   primary_key_query = """ SELECT  pg_attribute.attname,
                                 format_type(pg_attribute.atttypid, pg_attribute.atttypmod)
                         FROM    pg_index, pg_class, pg_attribute
@@ -23,4 +30,9 @@ for table in (table[0] for table in tables):
   cur = con.cursor()
   cur.execute(primary_key_query)
   keys = cur.fetchall()
-  print keys
+  for key in (key[0] for key in keys):
+    xml_primary_key = etree.SubElement(xmltable, "primaryKey")
+    xml_primary_key.text = key
+
+tree = ET.ElementTree(root)
+tree.write("schema.xml")
