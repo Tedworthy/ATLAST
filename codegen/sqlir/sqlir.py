d6dd7ba6632a18b7d08e62fc0ef3e9cb73d3ef31
Mitchell Allison <mitchellallison@me.com>
2013-11-08 15:09:44 +0000

    Working on and node

--- codegen/sqlir/sqlir.py
+++ codegen/sqlir/sqlir.py
@@ -22,19 +22,19 @@ class SQLIR():
       previous_constraint = self._constraint_stack.pop()
       conjunction = OrConstraint(previous_constraint, constraint)
 
-  def join_tree_equijoin(table, keys):
-    if len(self._join_tree) < 0:
-      self._join_tree.push(table)
-    else:
-      join = EquiJoin(self._join_tree, table, keys)
-      self._join_tree.push(join)
-
-  def join_tree_crossjoin(table):
-    if len(self._join_tree) < 0:
-      self._join_tree.push(table)
-    else:
-      join = CrossJoin(self._join_tree, table)
-      self._join_tree.push(join)
+#  def join_tree_equijoin(table, keys):
+#    if len(self._join_tree) < 0:
+#      self._join_tree.push(table)
+#    else:
+#      join = EquiJoin(self._join_tree, table, keys)
+#      self._join_tree.push(join)
+#
+#  def join_tree_crossjoin(table):
+#    if len(self._join_tree) < 0:
+#      self._join_tree.push(table)
+#    else:
+#      join = CrossJoin(self._join_tree, table)
+#      self._join_tree.push(join)
 
   def computeIR():
     # Invoke generic AST visitor here.
