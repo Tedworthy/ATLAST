b3ff6ee2e0a60fb9082c5b9941b774b33f54671d
Sam Esgate <sam.esgate11@imperial.ac.uk>
2013-11-08 15:10:27 +0000

    Added a skeleton file to begin the testing of our SQL generation.

---
+++ test/test_codegen.py
@@ -0,0 +1,13 @@
+from paste.fixture import TestApp
+from nose.tools import *
+
+class TestCodeGen():
+
+  def setup_func():
+
+  def teardown_func():
+
+  @with_setup(setup_func, teardown_func)
+  def sams_cheeky_test(self):
+    # Definitely should be some code here
+
