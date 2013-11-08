5b341e243107789038cc1670a4cfa33971fa4ccb
Mitchell Allison <mitchellallison@me.com>
2013-11-08 12:54:10 +0000

    Adding missing files

---
+++ codegen/constraint/binop_constraint.py
@@ -0,0 +1,10 @@
+class BinOpConstraint():
+  def __init__(self, left, right):
+    self._left = left
+    self._right = right
+
+  def getLeftConstraint(self):
+    return _left
+
+  def getRightConstraint(self):
+    return _right
