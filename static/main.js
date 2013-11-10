3b3a86c367291534cedf85f4f6bdd98ff26bb585
Ted Sales <ted@macrohard.co.uk>
2013-11-10 01:19:16 +0000

    Fix for the assumption where the automatic logic symbol replace-as-you-type code was assuming your cursor was at the end of the textarea. It now works throughout the textarea, in the way you'd intuitively expect. #awesome

--- static/main.js
+++ static/main.js
@@ -5,26 +5,40 @@ $(document).ready(function() {
     "or": "\u22C1",
     "implies": "\u2192",
     "iff": "\u2194",
-    "there_exists": "\u2203",
+    "exists": "\u2203",
     "forall": "\u2200"
   }
 
   var keys = {
-    "92": { "char": "\\", "formatters": ["and"] },
-    "47": { "char": "/", "formatters": ["or"] },
+    "92": { "char": "\\", "formatters": ["and", "or", "exists", "forall"] },
+    "47": { "char": "/", "formatters": ["or", "and"] },
+    "60": { "char": "<", "formatters": ["iff", "implies"] },
+    "45": { "char": "-", "formatters": ["iff", "implies"] },
     "62": { "char": ">", "formatters": ["iff", "implies"] },
-    "69": { "char": "E", "formatters": ["there_exists"] },
+    "69": { "char": "E", "formatters": ["exists"] },
     "65": { "char": "A", "formatters": ["forall"] }
   };
 
   var formatters = {
-    "and": { "regex": /\/\\/g, "result": unicode_chars.and },
-    "or": { "regex": /\\\//g, "result": unicode_chars.or },
-    "implies": { "regex": /->/g, "result": unicode_chars.implies },
-    "iff": { "regex": /<->/g, "result": unicode_chars.iff },
-    "there_exists": { "regex": /\\E/g, "result": unicode_chars.there_exists },
-    "forall": { "regex": /\\A/g, "result": unicode_chars.forall }
+    "and": { "regex": /\/\\/, "result": unicode_chars.and },
+    "or": { "regex": /\\\//, "result": unicode_chars.or },
+    "implies": { "regex": /->/, "result": unicode_chars.implies },
+    "iff": { "regex": /<->/, "result": unicode_chars.iff },
+    "exists": { "regex": /\\E/, "result": unicode_chars.exists },
+    "forall": { "regex": /\\A/, "result": unicode_chars.forall }
   };
+
+  // Extend String objects with custom regex match/search function
+  String.prototype.matchPortion = function(regex) {
+    var search = this.search(regex);
+    if (search == -1)
+      return null;
+    var matches = this.match(regex);
+    return {
+        "start": search,
+        "end": search + matches[0].length
+      };
+  }
 
   /* When 'Convert to SQL' button is clicked fire off an AJAX request */
   $("#convert_button").click(function() {
@@ -71,7 +85,7 @@ $(document).ready(function() {
       }
 
       // Add the resulting table to the page
-      $("#results_table").html(table);	
+      $("#results_table").html(table);
     });
     return false;
   });
@@ -86,70 +100,132 @@ $(document).ready(function() {
 
     var key = keys[k.toString()];
     if (key !== undefined) {
+      // Stop the keypress from happening normally
       event.preventDefault();
+
+      // Get the relevant formatters for the pressed key
       var matching_formatters = key.formatters.map(function(name) {
         return formatters[name];
       });
-      var logic = $(this).val() + key.char;
+
+      // Get current state
+      var cursor = $(this).getCursorPosition();
+      var logic = $(this).val();
+
+      // Augment the existing logic as if the pressed key had been inserted
+      logic = logic.substr(0, cursor) + key.char + logic.substr(cursor);
+      cursor += 1;
+
+      // Take (at most) a 10 character portion of the logic text around cursor
+      var portionStart = Math.max(0, cursor - 5);
+      var portionCursor = cursor - portionStart;
+      var portion = logic.substr(portionStart, 10);
+      var portionEnd = portionStart + portion.length;
+
+      // Go through all possible formatters
       $.each(matching_formatters, function(index, regex_result) {
-        logic = logic.replace(regex_result.regex, regex_result.result);
+        // Check for a match
+        portion_match = portion.matchPortion(regex_result.regex);
+        if (portion_match != null) {
+          // Match found - replace text to action the formatter
+          portion = portion.replace(regex_result.regex, regex_result.result);
+          // Update the cursor as a result
+          portionCursor = portion_match.start + 1;
+          // Break out of the loop - only one match allowed at the moment
+          return false;
+        }
       });
+
+      logic = logic.substr(0, portionStart) + portion
+              + logic.substr(portionEnd);
+      cursor = portionStart + portionCursor;
+
       $(this).val(logic);
+      $(this).setCursorPosition(cursor);
     }
   });
 
   // Buttons insert correct symbols
   $("#and_button").click(function() {
-    $("#logic").insertAtCaret(unicode_chars.and);
+    $("#logic").insertAtCursor(unicode_chars.and);
   });
 
   $("#or_button").click(function() {
-    $("#logic").insertAtCaret(unicode_chars.or);
+    $("#logic").insertAtCursor(unicode_chars.or);
   });
 
   $("#implies_button").click(function() {
-    $("#logic").insertAtCaret(unicode_chars.implies);
+    $("#logic").insertAtCursor(unicode_chars.implies);
   });
 
   $("#iff_button").click(function() {
-    $("#logic").insertAtCaret(unicode_chars.iff);
+    $("#logic").insertAtCursor(unicode_chars.iff);
   });
 
-  $("#there_exists_button").click(function() {
-    $("#logic").insertAtCaret(unicode_chars.there_exists);
+  $("#exists_button").click(function() {
+    $("#logic").insertAtCursor(unicode_chars.exists);
   });
 
   $("#forall_button").click(function() {
-    $("#logic").insertAtCaret(unicode_chars.forall);
+    $("#logic").insertAtCursor(unicode_chars.forall);
   });
 
-  // Insert symbols at cursor position (courtesy of StackOverflow)
+  // Add textarea cursor manipulation functions to jQuery
+  // All three functions based on code from StackOverflow
   $.fn.extend({
-    insertAtCaret: function(text) {
-      return this.each(function(i) {
-        if (document.selection) {
+    insertAtCursor:
+      function(text) {
+        var domElem = $(this).get(0);
+        if ('selection' in document) {
           // For browsers like Internet Explorer
-          this.focus();
+          domElem.focus();
           var sel = document.selection.createRange();
           sel.text = text;
-          this.focus();
-        } else if (this.selectionStart || this.selectionStart == '0') {
+          domElem.focus();
+        } else if ('selectionStart' in domElem || domElem.selectionStart == '0') {
           // For browsers like Firefox and WebKit-based
-          var startPos = this.selectionStart;
-          var endPos = this.selectionEnd;
-          var scrollTop = this.scrollTop;
-          this.value = this.value.substring(0, startPos) + text +
-            this.value.substring(endPos, this.value.length);
-          this.focus();
-          this.selectionStart = startPos + text.length;
-          this.selectionEnd = startPos + text.length;
-          this.scrollTop = scrollTop;
+          var startPos = domElem.selectionStart;
+          var endPos = domElem.selectionEnd;
+          var scrollTop = domElem.scrollTop;
+          domElem.value = domElem.value.substring(0, startPos) + text +
+            domElem.value.substring(endPos, domElem.value.length);
+          domElem.focus();
+          domElem.selectionStart = startPos + text.length;
+          domElem.selectionEnd = startPos + text.length;
+          domElem.scrollTop = scrollTop;
         } else {
-          this.value += text;
-          this.focus();
+          domElem.value += text;
+          domElem.focus();
         }
-      });
-    }
+      },
+    getCursorPosition:
+      function() {
+        var domElem = $(this).get(0);
+        var pos = 0;
+        if ('selectionStart' in domElem) {
+          pos = domElem.selectionStart;
+        } else if ('selection' in document) {
+          domElem.focus();
+          var sel = document.selection.createRange();
+          var selLength = document.selection.createRange().text.length;
+          sel.moveStart('character', -domElem.value.length);
+          pos = sel.text.length - selLength;
+        }
+        return pos;
+      },
+    setCursorPosition:
+      function(pos) {
+        var domElem = $(this).get(0);
+        if ('setSelectionRange' in domElem) {
+          domElem.setSelectionRange(pos, pos);
+        } else if ('createTextRange' in domElem) {
+          var range = domElem.createTextRange();
+          range.collapse(true);
+          range.moveEnd('character', pos);
+          range.moveStart('character', pos);
+          range.select();
+        }
+      }
   });
 
 });
