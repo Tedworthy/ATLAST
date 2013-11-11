50801af9301771680c4ca8af23282f6fc8f0393b
Max Tottenham <mtottenh@gmail.com>
2013-11-11 13:53:11 +0000

    might have fixed it

--- dbbackend/query.py
+++ dbbackend/query.py
@@ -17,7 +17,7 @@ def query(text):
     password = config.get('DatabaseCon', 'password')
     print "Host: " + host + "\tUser: " + user + "\tPassword: " + port 
     print "Password: " + password + "\tDatabase Name: "+ database
-    con = psycopg2.connect(host,port,database,user,password)
+    con = psycopg2.connect('host='+host+' port='+port+' databse='+database+' user='+user +' password='+password)
     cur = con.cursor()
     cur.execute(text)
     result = cur.fetchall()
