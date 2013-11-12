63d54599b8ddc433d6dd395528335847d8721b80
Sam Esgate <sam.esgate11@imperial.ac.uk>
2013-11-12 13:56:26 +0000

    SQLIR needs a redesign. Will go to this lecture and then sort out a nice interface.

--- codegen/sqlir/sqlir.py
+++ codegen/sqlir/sqlir.py
@@ -3,11 +3,44 @@ class SQLIR():
   def __init__(self):
     # An ordered list of field names (strings)
     self._select_set = []
+    # A tree containing the table names and fields to join on.
     self._join_tree = None #TODO
     self._constraint_tree = [] 
 
   def addSelectNode(self, node):
     self._select_set.add(node)
+
+
+  def computeIR(self):
+    # Invoke generic AST visitor here.
+    pass
+
+  def accept(self, visitor):
+    for select in self._select_set:
+
+    self._join_tree.accept(visitor)
+    for constraint in self._constraint_tree:
+      constraint.accept(visitor)
+    visitor.visit(self)
+
+  def __repr__(self):
+    string = "SQLIR: {\n"
+    string += "  Select Set: ["
+    select_strings = map(str, self._select_set)
+    string += ", ".join(select_strings)
+    string += "]\n"
+    string += "  Join Tree: "
+    string += str(self._join_tree)
+    string += "\n"
+    string += "  Constraint Stack: ["
+    constraint_strings = map(str, self._constraint_tree)
+    string += ", ".join(constraint_strings)
+    string += "]\n"
+    string += "}"
+    return string
+  
+  
+''' Things that don't make much sense 
 
   def constraint_tree_conjunction(constraint):
     if len(self._constraint_tree) < 0:
@@ -37,30 +70,4 @@ class SQLIR():
 #      join = CrossJoin(self._join_tree, table)
 #      self._join_tree.push(join)
 
-  def computeIR(self):
-    # Invoke generic AST visitor here.
-    pass
-
-  def accept(self, visitor):
-    for select in self._select_set:
-
-    self._join_tree.accept(visitor)
-    for constraint in self._constraint_tree:
-      constraint.accept(visitor)
-    visitor.visit(self)
-
-  def __repr__(self):
-    string = "SQLIR: {\n"
-    string += "  Select Set: ["
-    select_strings = map(str, self._select_set)
-    string += ", ".join(select_strings)
-    string += "]\n"
-    string += "  Join Tree: "
-    string += str(self._join_tree)
-    string += "\n"
-    string += "  Constraint Stack: ["
-    constraint_strings = map(str, self._constraint_tree)
-    string += ", ".join(constraint_strings)
-    string += "]\n"
-    string += "}"
-    return string
+'''
