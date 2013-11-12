8474e89c0de6792515be5e9b4bfbff39daf55839
Tom Harling <thomas.harling11@imperial.ac.uk>
2013-11-12 21:31:21 +0000

    inner loop

--- static/main.js
+++ static/main.js
@@ -15,7 +15,9 @@ $(document).ready(function() {
       output += '<p>Table ' + table + ' has columns ';
       
       $.each(p_keys, function(text, keys) {
+        $.each(keys, function(key) {
         
+        });
           
         
       });
