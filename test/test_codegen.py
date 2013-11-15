246bc007bfb0b00723755a0ba175bd5edf272ddf
Sam Esgate <sam.esgate11@imperial.ac.uk>
2013-11-15 12:58:26 +0000

    Changes to test_codegen make it more likely to run but still not currently working.

--- test/test_codegen.py
+++ test/test_codegen.py
@@ -5,7 +5,7 @@ This file contains tests for the translation from the parse tree to sql.
 NOTE: It assumes that the parser works correctly.
 '''
 
-import parsing
+from parsing.lexer import *
 from paste.fixture import TestApp
 from nose.tools import *
 
@@ -16,14 +16,7 @@ class TestCodeGen():
 
   def teardown_func():
     pass
-
-  @with_setup(setup_func, teardown_func)
-  def select_1_from_1(self):
-    logic = t_forAll + "x(film_title(x, y))"
-    sql = "SELECT title FROM film"
-    
-    assert(translates_to(logic, sql))
-
+  
   def translates_to(logicString, expectedSQLString):
     # Create a Logic Tree from the Logic
     logicTree = parsing.parse_input(logicString)
@@ -45,8 +38,17 @@ class TestCodeGen():
     convertedSQLString = sqlIR.getSQL()
    
     return true
+
     # Run converted and expected SQL queries and compare results
     # convertedResult = RUN_SQL(convertedSQLString)
     # expectedResult = RUN_SQL(expectedSQLString)
 
     # return convertedResult == expectedResult
+
+  @with_setup(setup_func, teardown_func)
+  def test_select_1_from_1(self):
+    logic = t_FORALL + "x(film_title(x, y))"
+    sql = "SELECT title FROM film"
+    
+    assert(translates_to(logic, sql))
+
