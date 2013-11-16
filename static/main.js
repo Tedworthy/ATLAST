407d3e08531e658a0b0630320e482f58ed158508
Ted Sales <ted@macrohard.co.uk>
2013-11-16 18:32:19 +0000

    Hopefully pulled through all my changes that got nuked.

--- static/main.js
+++ static/main.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
   });
 
   var schema;
-    
+
   $.ajax({
     type: "GET",
-    url: "/schema"    
+    url: "/schema"
   }).done(function(result) {
     // See generate_schema.py for the original query
     schema = $.parseJSON(result);
@@ -17,7 +17,7 @@ $(document).ready(function() {
 
     // For each table...
     $.each(schema, function(table, p_keys) {
-      output += '<p>Table ' + table + ' has primary keys: ';unifcuniasdasdasasfaeg
+      output += '<p>Table ' + table + ' has primary keys: ';
 
       // p_keys is the [(primary keys object), (headings objects)]
       $.each(p_keys, function(text, keys) {
@@ -47,41 +47,40 @@ $(document).ready(function() {
     "and": "\u2227",
     "or": "\u2228",
     "implies": "\u2192",
-    "there_exists": "\u2203",
+    "iff": "\u2194",
+    "exists": "\u2203",
     "forall": "\u2200",
-    "not": "\u00AC",
-    "iff": "\u2194"
+    "not": "\u00AC"
   }
 
   var keys = {
-    "92": { "char": "\\", "formatters": ["and"] },
-    "47": { "char": "/", "formatters": ["or"] },
-    "62": { "char": ">", "formatters": ["implies"] },
-    "69": { "char": "E", "formatters": ["there_exists"] },
+    "92": { "char": "\\", "formatters": ["and", "or", "exists", "forall"] },
+    "47": { "char": "/", "formatters": ["or", "and"] },
+    "60": { "char": "<", "formatters": ["iff", "implies"] },
+    "45": { "char": "-", "formatters": ["iff", "implies"] },
+    "62": { "char": ">", "formatters": ["iff", "implies"] },
+    "69": { "char": "E", "formatters": ["exists"] },
     "65": { "char": "A", "formatters": ["forall"] }
   };
 
   var formatters = {
-    "and": { "regex": /\/\\/g, "result": unicode_chars.and },
-    "or": { "regex": /\\\//g, "result": unicode_chars.or },
-    "implies": { "regex": /->/g, "result": unicode_chars.implies },
-    "there_exists": { "regex": /\\E/g, "result": unicode_chars.there_exists },
-    "forall": { "regex": /\\A/g, "result": unicode_chars.forall },
-    "iff": { "regex": /<>/g, "result": unicode_chars.iff }
+    "and": { "regex": /\/\\/, "result": unicode_chars.and },
+    "or": { "regex": /\\\//, "result": unicode_chars.or },
+    "implies": { "regex": /->/, "result": unicode_chars.implies },
+    "iff": { "regex": /<->/, "result": unicode_chars.iff },
+    "exists": { "regex": /\\E/, "result": unicode_chars.exists },
+    "forall": { "regex": /\\A/, "result": unicode_chars.forall }
   };
+
   $("#config_submit").click(function()  {
     user = $("#user_input").val();
-
     $.post(
       "/login", "i like cheese"
-      
-        
     /*    user : user,
         password : $("#password_input").val(),
         host : $("#host_input").val(),
         port : $("#port_input").val(),
         dbname : $("#dbname_input").val() */
-      
     ).done(function(result) {
       var response = $.parseJSON(result);
       if (response.error === 'ok') {
@@ -93,12 +92,12 @@ $(document).ready(function() {
       }
     });
   });
+
   /* When 'Convert to SQL' button is clicked fire off an AJAX request */
   $("#convert_button").click(function() {
     var input_string = $("textarea#logic").val();
     $.ajax({
       type: "POST",
-
       data: {
         "logic" : input_string
       }
@@ -113,113 +112,174 @@ $(document).ready(function() {
       } else {
         $("textarea#sql_result").text(response.error);
       }
-      
+
       $("textarea#query_result").text(JSON.stringify(response.query) + "\n");
-      
+
       // Print rows from result of running query on database
-      var table='<table border="1" align="center"> <tr>';
+      var table = '<table border="1" align="center"> <tr>';
       var first_row = response.query[0];
       $.each(first_row, function(k, v) {
         table += '<th>' + k + '</th>';
       });
       table += '</tr>';
-      
+
       // loop over each object in the array to create rows
       $.each(response.query, function() {
-      table += '<tr>'
+        table += '<tr>';
         $.each(this, function(k, v) {
           table += ('<td>' + v + '</td>');
         });
-        
         table += '</tr>'
       });
-      
+
       // Add the resulting table to the page
-      $("#results_table").html(table);	
-      
+      $("#results_table").html(table);
     });
+
     return false;
   });
 
   // Convert characters to correct symbols
   $("textarea#logic").keypress(function(event) {
     console.log("Key down:" + event.keyCode);
-    
+
     // Firefox / Chrome compatibility
     var k = (typeof event.which === "number") ? event.which : event.keyCode;
-    
+
     var key = keys[k.toString()];
     if (key !== undefined) {
+      // Stop the keypress from happening normally
       event.preventDefault();
+
+      // Get the relevant formatters for the pressed key
       var matching_formatters = key.formatters.map(function(name) {
         return formatters[name];
       });
-      var logic = $(this).val() + key.char;
-      $.each(matching_formatters, function(index, regex_result) {
-        logic = logic.replace(regex_result.regex, regex_result.result);
+
+      // Get current state
+      var cursor = $(this).getCursorPosition();
+      var logic = $(this).val();
+
+      // Augment the existing logic as if the pressed key had been inserted
+      logic = logic.substr(0, cursor) + key.char + logic.substr(cursor);
+      cursor += 1;
+
+      // Take (at most) a 10 character portion of the logic text around cursor
+      var portionStart = Math.max(0, cursor - 5);
+      var portionCursor = cursor - portionStart;
+      var portion = logic.substr(portionStart, 10);
+      var portionEnd = portionStart + portion.length;
+
+      // Go through all the possible formatters
+      $.each(matching_formatters, function(index, regexResult) {
+        // Check for a match
+        portionMatch = portion.search(regexResult.regex);
+        if (portionMatch != -1) {
+          // Match found - replace text to action the formatter
+          portion = portion.replace(regexResult.regex, regexResult.result);
+          // Update the cursor as a result
+          portionCursor = portionMatch + 1;
+          // Break out of the loop - only one match allowed at the moment
+          return false;
+        }
       });
+
+      // Put the modified portion back into the logic text
+      logic = logic.substr(0, portionStart) + portion +
+              logic.substr(portionEnd);
+      cursor = portionStart + portionCursor;
+
+      // Update the UI
       $(this).val(logic);
+      $(this).setCursorPosition(cursor);
     }
   });
 
   // Buttons insert correct symbols
   $("#and_button").click(function() {
-    $("#logic").insertAtCaret(unicode_chars.and);
+    $("#logic").insertAtCursor(unicode_chars.and);
   });
 
   $("#or_button").click(function() {
-    $("#logic").insertAtCaret(unicode_chars.or);
+    $("#logic").insertAtCursor(unicode_chars.or);
   });
-  
+
   $("#implies_button").click(function() {
-    $("#logic").insertAtCaret(unicode_chars.implies);
+    $("#logic").insertAtCursor(unicode_chars.implies);
   });
-  
-  $("#there_exists_button").click(function() {
-    $("#logic").insertAtCaret(unicode_chars.there_exists);
-  });
-  
-  $("#forall_button").click(function() {
-    $("#logic").insertAtCaret(unicode_chars.forall);
-  });
- 
-  $("#not_button").click(function() {
-    $("#logic").insertAtCaret(unicode_chars.not);
-  });
-  
+
   $("#iff_button").click(function() {
-    $("#logic").insertAtCaret(unicode_chars.iff);
+    $("#logic").insertAtCursor(unicode_chars.iff);
   });
-  
-  
+
+  $("#exists_button").click(function() {
+    $("#logic").insertAtCursor(unicode_chars.exists);
+  });
+
+  $("#forall_button").click(function() {
+    $("#logic").insertAtCursor(unicode_chars.forall);
+  });
+
+  $("#not_button").click(function() {
+    $("#logic").insertAtCursor(unicode_chars.not);
+  });
 
   // Insert symbols at cursor position
   $.fn.extend({
-    insertAtCaret: function(text) {
-      return this.each(function(i) {
-        if (document.selection) {
+    insertAtCursor:
+      function(text) {
+        var domElem = $(this).get(0);
+        if ('selection' in document) {
           // For browsers like Internet Explorer
-          this.focus();
+          domElem.focus();
           var sel = document.selection.createRange();
           sel.text = text;
-          this.focus();
-        } else if (this.selectionStart || this.selectionStart == '0') {
+          domElem.focus();
+        } else if ('selectionStart' in domElem ||
+                   domElem.selectionStart == '0') {
           // For browsers like Firefox and Webkit based
-          var startPos = this.selectionStart;
-          var endPos = this.selectionEnd;
-          var scrollTop = this.scrollTop;
-          this.value = this.value.substring(0, startPos) + text +
-            this.value.substring(endPos, this.value.length);
-          this.focus();
-          this.selectionStart = startPos + text.length;
-          this.selectionEnd = startPos + text.length;
-          this.scrollTop = scrollTop;
+          var startPos = domElem.selectionStart;
+          var endPos = domElem.selectionEnd;
+          var scrollTop = domElem.scrollTop;
+          domElem.value = domElem.value.substring(0, startPos) + text +
+            domElem.value.substring(endPos, domElem.value.length);
+          domElem.focus();
+          domElem.selectionStart = startPos + text.length;
+          domElem.selectionEnd = startPos + text.length;
+          domElem.scrollTop = scrollTop;
         } else {
-          this.value += text;
-          this.focus();
+          domElem.value += text;
+          domElem.focus();
         }
-      });
-    }
+      },
+    getCursorPosition:
+      function() {
+        var domElem = $(this).get(0);
+        var pos = 0;
+        if ('selectionStart' in domElem) {
+          pos = domElem.selectionStart;
+        } else if ('selection' in document) {
+          domElem.focus();
+          var sel = document.selection.createRange();
+          var selLength = document.selection.createRange().text.length;
+          sel.moveStart('character', -domElem.value.length);
+          pos = sel.text.length - selLength;
+        }
+        return pos;
+      },
+    setCursorPosition:
+      function(pos) {
+        var domElem = $(this).get(0);
+        if ('setSelectionRange' in domElem) {
+          domElem.setSelectionRange(pos, pos);
+        } else if ('createTextRange' in domElem) {
+          var range = domElem.createTextRange();
+          range.collapse(true);
+          range.moveEnd('character', pos);
+          range.moveStart('character', pos);
+          range.select();
+        }
+      }
   });
 
 });
