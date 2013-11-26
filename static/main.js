2c5b5211a57920fc8870422b7db6bf55136660e1
Tom Harling <thomas.harling11@imperial.ac.uk>
2013-11-26 21:25:20 +0000

    removed parseJson stuff

--- static/main.js
+++ static/main.js
@@ -8,10 +8,9 @@ $(document).ready(function() {
   $.ajax({
     type: "GET",
     url: "/schema"
-  }).done(function(result) {
+  }).done(function(schema) {
     // See generate_schema.py for the original query
-    schema = $.parseJSON(result);
-    //alert('the schema is ' + schema);
+    
     // Print out the name of each table and their primary keys
     output = '';
 
@@ -84,7 +83,7 @@ $(document).ready(function() {
         dbname : $("#dbname_input").val() }
     ).done(function(result) {
 
-     var response = $.parseJSON(result);
+     var response = result;
       if (response.error === 'ok') {
         var n = noty({text: 'Configuration Accepted'})
         $(".close").trigger("click")
