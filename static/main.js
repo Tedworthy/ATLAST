20dbd003447a1a0408bd0089e2362ad6143bb5a5
Tom Harling <thomas.harling11@imperial.ac.uk>
2013-11-27 20:29:14 +0000

    bracket fail

--- static/main.js
+++ static/main.js
@@ -125,7 +125,7 @@ $(document).ready(function() {
           var sql_result = response.sql.concat("\n\n\nDatabase error message:\n", response.error);
           //var sql_result_lines = sql_result.split("\n");  
           //alert(sql_result_lines.length);
-          $("textarea#sql_result").text(sql_result));
+          $("textarea#sql_result").text(sql_result);
           $("textarea#sql_result").css("height", "200px");
           $("#results_table").html("");
         }
