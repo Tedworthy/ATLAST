b79a3bb02ff7eb65d6773c778ee530131dccf42c
Mitchell Allison <mitchellallison@me.com>
2014-01-09 15:50:59 +0000

    ThereExists now works, with the slight annoyance that free variables from inner queries cannot be projected

--- test/test_codegen.py
+++ test/test_codegen.py
@@ -382,5 +382,7 @@ class TestCodeGen():
   # "Get me all the films such that another film exists, and return that too!."
   def test_there_exists_inner(self):
     logic = "∃x(films_title(x, title1) ∧ ∃y(films_title(y, title2) ∧ title1 != title2))".decode('utf8')
-    sql = "SELECT films1.title, films2.title2 FROM films1 WHERE EXISTS (SELECT films2.title FROM films2 WHERE films1.title != films2.title)"
+    sql = "SELECT films1.title FROM films AS films1 WHERE EXISTS"
+    sql += " (SELECT films2.title FROM films AS films2 WHERE films1.title <> "
+    sql += "films2.title)"
     assert self.translates_to(logic, sql), "Error, expected answers not equal"
