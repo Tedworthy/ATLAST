4a542d9d578e44062c154918e87303ec03158b45
Mitchell Allison <mitchellallison@me.com>
2014-01-09 14:10:58 +0000

    Still working on existential quantifiers. Need to implement generation in the SQLGenerator

--- codegen/ir/__init__.py
+++ codegen/ir/__init__.py
@@ -4,6 +4,7 @@ from and_constraint import AndConstraint
 from binop_constraint import BinOpConstraint
 from null_node import NullNode
 from unary_constraint import UnaryConstraint
+from exists_constraint import ExistsConstraint
 from relation_attribute_pair import RelationAttributePair
 
 from equijoin_node import EquiJoinNode
